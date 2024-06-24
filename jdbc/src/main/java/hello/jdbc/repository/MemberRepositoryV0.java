ccec227e8f1a7b4e7456f592fa24137b7cd9cdc3
yaro <43820977+yootaebong@users.noreply.github.com>
2024-06-24 12:08:42 +0900

    feat : JDBC 개발 - 수정, 삭제

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -25,6 +26,72 @@ public class MemberRepositoryV0 {
         } catch (Exception e) {
             log.error("error = {}", e);
             throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId){
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                return new Member(rs.getString("member_id"), rs.getInt("money"));
+            }
+            else{
+                throw new NoSuchElementException("No value member_id = " + memberId);
+            }
+        } catch (Exception e) {
+            log.error("error = {}", e);
+            throw new IllegalStateException(e);
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    public void update(String memberId, int money){
+        String sql = "update member set money = ? where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int result = pstmt.executeUpdate();
+            log.info("result = {}", result);
+        } catch (Exception e) {
+            log.error("error = {}", e);
+            throw new IllegalStateException(e);
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public void delete(String memberId){
+        String sql = "delete from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            log.error("error = {}", e);
+            throw new IllegalStateException(e);
         } finally {
             close(con, pstmt, null);
         }
--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,20 +1,38 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
-    void save() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+    void crud() throws SQLException {
+        String memberId = "memberV102";
+
+        Member member = new Member(memberId, 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(memberId);
+        assertEquals(member.getMemberId(), findMember.getMemberId());
+
+        // update money 10000 -> 20000
+        repository.update(findMember.getMemberId(), 20000);
+        Member updateMember = repository.findById(findMember.getMemberId());
+        assertEquals(20000, updateMember.getMoney());
+
+        // delete
+        repository.delete(findMember.getMemberId());
+        assertThrows(IllegalStateException.class, () -> repository.findById(findMember.getMemberId()));
     }
+
 
 }
