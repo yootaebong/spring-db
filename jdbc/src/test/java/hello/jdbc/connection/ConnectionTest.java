5ce8368b09aebc6b1a32ae42b7a69b656ca26b11
yaro <43820977+yootaebong@users.noreply.github.com>
2024-06-24 14:24:05 +0900

    feat : DataSource 에제2 - 커넥션 풀

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -26,6 +27,19 @@ public class ConnectionTest {
     void dataSourceDriverManager() throws SQLException {
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSorce(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSorce(dataSource);
+        Thread.sleep(2000);
     }
 
 
